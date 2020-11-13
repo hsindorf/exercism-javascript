a05250120789c2026af7f56ca7a12056946c7fe5
Hannah Sindorf <hannah@hsindorf.com>
2020-11-12 23:12:41 -0800

    solve with crappy nested logic

--- leap/leap.js
+++ leap/leap.js
@@ -1,3 +1,12 @@
 export const isLeap = (year) => {
-
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      if (year % 400 === 0) {
+        return true;
+      }
+      return false;
+    }
+    return true;
+  }
+  return false;
 };
