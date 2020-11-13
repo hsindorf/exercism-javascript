e42ce493d48d084af23514eddb60017c7bbe2937
Hannah Sindorf <hannah@hsindorf.com>
2020-11-12 22:31:12 -0800

    solve roman numerals

--- roman-numerals/roman-numerals.js
+++ roman-numerals/roman-numerals.js
@@ -1,8 +1,52 @@
-//
-// This is only a SKELETON file for the 'Roman Numerals' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const toRoman = (num) => {
+  let position = 8;
+  let remainingNum = num;
+  let output = "";
 
-export const toRoman = () => {
-  throw new Error("Remove this statement and implement this function");
+  for (let i = 1000; i >= 1; i/= 10) {
+    const multipleOfI = Math.floor(remainingNum / i);
+
+    output += toRomanIndiv(UNITS[position], UNITS[position -= 1], UNITS[position -= 1], multipleOfI);
+
+    remainingNum -= multipleOfI * i;
+  }
+
+  return output;
 };
+
+const toRomanIndiv = (ten, five, one, num) => {
+  switch (num) {
+    case 1: {
+      return `${one}`;
+    }
+    case 2: {
+      return `${one}${one}`;
+    }
+    case 3: {
+      return `${one}${one}${one}`;
+    }
+    case 4: {
+      return `${one}${five}`;
+    }
+    case 5: {
+      return `${five}`;
+    }
+    case 6: {
+      return `${five}${one}`;
+    }
+    case 7: {
+      return `${five}${one}${one}`;
+    }
+    case 8: {
+      return `${five}${one}${one}${one}`
+    }
+    case 9: {
+      return `${one}${ten}`
+    }
+    default: {
+      return "";
+    }
+  }
+};
+
+const UNITS = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
