5878371f76f4c24c3e8692638657523367e09de7
Hannah Sindorf <hannah@hsindorf.com>
2020-11-14 18:52:59 -0800

    triangle

--- triangle/triangle.js
+++ triangle/triangle.js
@@ -1,22 +1,43 @@
-//
-// This is only a SKELETON file for the 'Triangle' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class Triangle {
-  constructor() {
-    throw new Error("Remove this statement and implement this function");
+  constructor(sideA, sideB, sideC) {
+    this.sideA = sideA;
+    this.sideB = sideB;
+    this.sideC = sideC;
   }
 
   isEquilateral() {
-    throw new Error("Remove this statement and implement this function");
+    return (
+      this.isValid() && this.sideA == this.sideB && this.sideB == this.sideC
+    );
   }
 
   isIsosceles() {
-    throw new Error("Remove this statement and implement this function");
+    return (
+      this.isValid() &&
+      (this.sideA == this.sideB ||
+        this.sideB == this.sideC ||
+        this.sideC == this.sideA)
+    );
   }
 
   isScalene() {
-    throw new Error("Remove this statement and implement this function");
+    return (
+      this.isValid() &&
+      this.sideA !== this.sideB &&
+      this.sideB !== this.sideC &&
+      this.sideC !== this.sideA
+    );
+  }
+
+  // this is in its own function vs. the constructor because of how the tests are written
+  isValid() {
+    return (
+      this.sideA !== 0 &&
+      this.sideB !== 0 &&
+      this.sideC !== 0 &&
+      this.sideA + this.sideB > this.sideC &&
+      this.sideA + this.sideC > this.sideB &&
+      this.sideB + this.sideC > this.sideA
+    );
   }
 }
